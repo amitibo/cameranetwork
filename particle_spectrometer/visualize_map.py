a25f812b7aa0bf89cb91f7e840f4384b19f1eacc
EED\amitibo <amitibo@campus.technion.ac.il>
2017-07-16 22:22:03 +0300

    Animation of path not working.

--- particle_spectrometer/visualize_map.py
+++ particle_spectrometer/visualize_map.py
@@ -99,25 +99,69 @@ def convertMapData(lat, lon, hgt, lat0=32.775776, lon0=35.024963, alt0=229):
     return X, Y, Z, Z_mask
 
 
-def draw_map(mayavi_scene, map_coords):
-    """Clear the map view and draw elevation map."""
+class Visualization(object):
 
-    mlab.clf(figure=mayavi_scene)
-    X, Y, Z, Z_mask = convertMapData(
-        map_coords[0],
-        map_coords[1],
-        map_coords[2],
-    )
+    def __init__(self, map_coords, x, y, z, s):
 
-    mlab.mesh(Y, X, MAP_ZSCALE * Z, figure=mayavi_scene, mask=Z_mask, color=(0.7, 0.7, 0.7))
+        self.x = x
+        self.y = y
+        self.z = z
+        self.s = s
+
+        self.animation_index = 2
+
+        #
+        # Setup the map.
+        #
+        self.mayavi_scene = mlab.figure(
+            bgcolor=(1, 1, 1),
+            size=(900, 900)
+        )
+        #self.draw_map(map_coords)
+        self.draw_path()
+
+    def draw_map(self, map_coords):
+        """Clear the map view and draw elevation map."""
+
+        mlab.clf(figure=self.mayavi_scene)
+        X, Y, Z, Z_mask = convertMapData(
+            map_coords[0],
+            map_coords[1],
+            map_coords[2],
+        )
+
+        mlab.mesh(Y, X, MAP_ZSCALE * Z, figure=self.mayavi_scene, mask=Z_mask, color=(0.7, 0.7, 0.7))
+
+    def draw_path(self):
+
+        self.pts3d = mlab.points3d(
+            self.x,
+            self.y,
+            MAP_ZSCALE*self.z,
+            self.s
+        )
+
+    def update_pts3d(self):
+        """Update the animation of the path."""
+
+        self.animation_index += 1
+        mask = np.zeros_like(self.x, dtype=np.bool)
+        mask[:self.animation_index] = True
+
+        self.pts3d.mlab_source.reset(
+            x=self.x[mask],
+            y=self.y[mask],
+            z=self.z[mask],
+            scalars=self.s[mask]
+        )
 
 
-@mlab.animate(delay=100)
+@mlab.animate(delay=50)
 def anim():
     f = mlab.gcf()
     while 1:
-        f.scene.camera.azimuth(2)
-        f.scene.render()
+        f.scene.camera.azimuth(1)
+        vis.update_pts3d()
         yield
 
 
@@ -129,20 +173,27 @@ def main():
     s = df[PARTICLE_SIZE].values
 
     #
-    # Setup the map.
-    #
-    map_coords = loadMapData()
-    mayavi_scene = mlab.figure(bgcolor=(1, 1, 1), size=(900, 900))
-    draw_map(mayavi_scene, map_coords)
-
-    #
-    # Setup to the path.
+    # Mask the path.
     #
     s[s>5000] = 5000
     mask = (x>-10000) & (x<10000) & (y>-10000) & (y<10000)
-    mlab.points3d(x[mask], y[mask], MAP_ZSCALE * z[mask], s[mask])
+    x, y, z, s = x[mask], y[mask], z[mask], s[mask]
 
-    a = anim()
+    #
+    # Load the map.
+    #
+    map_coords = loadMapData()
+
+    #
+    # Create the visualization.
+    #
+    global vis
+    vis = Visualization(map_coords, x, y, z, s)
+    animation = anim()
+
+    #
+    # Start.
+    #
     mlab.show()
 
 
