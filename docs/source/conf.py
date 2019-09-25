5a9922c658e9c04086d93a94d0bb342c2cc11f1b
Adi Vainiger <adi.vainiger@gmail.com>
2019-09-25 19:49:43 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,7 @@ sys.path.insert(0, os.path.abspath('../../../../.local/lib/python2.7/site-packag
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.numfig',
+#     'sphinx.ext.numfig', # Commented so that readthedocs will work
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
